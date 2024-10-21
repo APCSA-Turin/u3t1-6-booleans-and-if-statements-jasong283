0bb7d0cc17003cfcbd1784044f26543b0cb271ea
jasong283 <jasong283@nycstudents.net>
2024-10-21 16:13:56 +0000

    commit

---
+++ L1/Runner.java
@@ -0,0 +1,26 @@
+package L1;
+import java.util.Scanner;
+
+public class Runner {
+   public static void main(String[] args) {
+
+    int x = 10;
+    int y = x;
+    if (x > 5) {
+        if (x > 10) {
+            y--;
+        }
+    } else {
+        int z = x;
+        if (y >= z) {
+            z++;
+        } else {
+            z--;
+        }
+    }
+
+
+
+   }
+}
+
---
+++ NumberStuff.java
@@ -0,0 +1,14 @@
+public class NumberStuff {
+
+   public NumberStuff() { }
+
+   public String positiveZeroOrNegative(int num) {
+       if (num > 0) {
+           return num + " is a positive number";
+       } else if(num < 0) {
+           return num + " is a negative number";
+       } else {
+        return num + " isn't positive or negative";
+       }
+   }
+}
